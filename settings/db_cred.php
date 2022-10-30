3e1029251e20c7014aa6b86d5f880152cc37df06
Yemofio <95033608+Yemofio@users.noreply.github.com>
2022-10-30 21:36:21 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -8,7 +8,7 @@
 // Heroku Database credentials
 define('SERVER', 'us-cdbr-east-06.cleardb.net');
 define('USERNAME','b9cd28c7abc157');
-define('PASSWD', 'd5fb23f2');
+define('PASSWORD', 'd5fb23f2');
 define('DATABASE', 'heroku_0486fd9877d15e7');
 
 ?>
